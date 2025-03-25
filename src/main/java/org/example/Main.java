c364464c767e660623d117c6e23f0c16033fb6c3
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-03-25 10:08:26 -0400

    first commit - added showArray

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+    public static void showArray(int[] a) {
+       for(int i = 0; i < a.length; i++) {
+           System.out.println(a[i]);
+       }
+    }
+
+    public static int[] countingsort(int[] in, int k) {
+        int[] countArray = new int[k + 1];
+    }
+
+
+
+
+
+ }
